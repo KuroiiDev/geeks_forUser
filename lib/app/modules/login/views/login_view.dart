d76e0a17ef0fc90ee25a82641f32ecaf97e7988c
KuroiiDev <jorryc01@email.com>
2024-01-25 13:45:50 +0700

    Styling Login nya cuyyy

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:geek/app/widgets/custom_clipper.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../controllers/login_controller.dart';
 
@@ -23,6 +24,9 @@ class LoginView extends GetView<LoginController> {
       )
     );
   }
+
+  //Personal Widget..
+
   Widget _buildInputFields(){
     return ClipPath(
       clipper: CustomClipperWidget(),
@@ -43,10 +47,39 @@ class LoginView extends GetView<LoginController> {
                 image: AssetImage("assets/images/logo_white.png"),
                 height: Get.height * 0.2,
               ),
-              Text("Login", style: TextStyle(
-                color: Colors.white,
-
-                ),
+              Text(
+                  "Welcome Back Geek!",
+                  style:GoogleFonts.creteRound(
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 1
+                  )
+              ),
+              SizedBox(height: 60),
+              Form(
+                key: controller.formKey,
+                  child:Column(
+                    children: [
+                      _buildTextFormField(controller.emailController, Icons.person, "Email", "Email Can't Be Empty!"),
+                      SizedBox(height: 20),
+                      _buildTextFormField(controller.passwordController, Icons.lock, "Password", "Password Can't Be Empty!", isPassword: true),
+                      SizedBox(height: 30),
+                      ElevatedButton(
+                        onPressed: (){},
+                        child: Text("Login", style: GoogleFonts.alata(color: Colors.white, fontSize: 25)),
+                      ),
+                      SizedBox(height: 15),
+                      TextButton(
+                          onPressed: (){},
+                          child:
+                          Text(
+                              "Forgot your Password? click here to reset!",
+                              style: GoogleFonts.alata(color: Colors.white70, fontSize: 17)
+                          )
+                      )
+                    ],
+                  )
               ),
             ],
           ),
@@ -54,20 +87,31 @@ class LoginView extends GetView<LoginController> {
       ),
     );
   }
+  Widget _buildSocialLogins(){
+    return Container();
+  }
+  Widget _buildTextFormField(TextEditingController controller, IconData icon, String hint, String ifEmpty,{bool isPassword = false}){
+    return TextFormField(
+      controller: controller,
+      style: GoogleFonts.alata(color: Colors.white, fontSize: 20),
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: GoogleFonts.alata(color: Colors.white.withOpacity(0.4), fontSize: 20),
+        prefixIcon: Icon(
+          icon,
+          color: Colors.white,
+        ),
+        filled: true,
+        fillColor: Colors.white.withOpacity(0.1),
+        border: OutlineInputBorder(borderSide: BorderSide.none),
+      ),
+      obscureText: isPassword,
+      validator: (value){
+        if (value!.isEmpty){
+          return ifEmpty;
+        }
+        return null;
+      },
+    );
+  }
 }
-/*
-    decoration: BoxDecoration(
-        gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [Color(0xffe18bff), Color(0xff7000FF)]
-        )
-    ),
-    child: Scaffold(
-    backgroundColor: Colors.transparent,
-    body: Stack(
-    children: [
-    Image(image: AssetImage("assets/images/logo_white.png"))
-    ],
-    ),
-    ),*/
